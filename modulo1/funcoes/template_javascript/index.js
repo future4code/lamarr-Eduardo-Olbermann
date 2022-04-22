da1ea294903a603bb039bd6450a62d95a2280a65
EduardoFlossOlbermann <eduardoolbermann@hotmail.com>
2022-04-22 17:37:56 -0300

    projeto-javascript

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,23 @@
+// 1. Exercicio interpretação de código
+
+// a.10,50
+// b.Ele não imprimiria o resultado no console, ele iria fazer a multiplicação normal.
+
+// 2. Leia o código abaixo
+
+// a.Ele pediu pro usuário digitar um texto, converteu o texto que o usuario digitou em letras minusculas
+//e verificou se o usuario inseriu a palavra cenoura, retornando um booleano.
+// (?)
+
+// b. i.Eu gosto de cenoura = true
+// ii.CENOURA é bom pra vista = true
+// iii.Cenouras crescem na terra = true
+
+// 1. Exercícios de escrita de código
+// a.
+
+console.log("Eu sou Eduardo, tenho 21 anos, moro em Santa Catarina e sou estudante.")
+
+// b.
+
+
