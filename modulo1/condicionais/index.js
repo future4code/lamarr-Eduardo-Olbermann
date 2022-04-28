edd806dce95f813057776f3e1f0dbb6d98e727b2
EduardoFlossOlbermann <eduardoolbermann@hotmail.com>
2022-04-28 19:28:47 -0300

    exercicios-condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,83 @@
+// 1. Exercícios de interpretação de código
+
+// a. Ele verifica se o número que o usuário digitar e par ou impar, se for par, ele vai dar ''passou no teste'' e se for impar ele vai ser ''não passou no teste''.
+// b. Para números pares
+// c. Para números impares 
+
+// 2. 
+
+// a. Para saber o preço de uma determinada fruta que o usuário digitar.
+// b. "o preço da fruta maçã é de R$ 2.25"
+// c. o código vai quebrar e vai mostrar o valor que está no default.
+
+// 3.
+
+// a. Ele está pedindo pro usuário digitar um número 
+// b. Ele responderia no console a mensagem ''Esse número passou no teste'', pois o número q o usuário digitar ele vai verificar se e maior que 0.
+// c. Sim, pois não tem o ''else'' embaixo para dar a resposta se o número não for maior que 0.
+
+
+//  1. Exercícios de escrita de código
+
+// let idadeUsuario = Number(prompt("Qual sua idade?"))
+// let recebeIdade = idadeUsuario
+
+// if (recebeIdade > 18){
+//     console.log("Você pode dirigir")
+// }else{
+//     console.log("Você não pode dirigir")
+// }
+
+// 2.
+
+// let turnoDoAluno = prompt("Em qual turno você estuda? Digite: M para Matutino, V para Vespertino ou N para Noturno")
+
+// if (turnoDoAluno === "M") {
+//     console.log("Bom dia")
+// } else if (turnoDoAluno === "V") {
+//     console.log("Boa tarde")
+// } else if (turnoDoAluno === "N") {
+//     console.log("Boa noite")
+// }
+
+// 3. 
+
+// let turnoDoAlunoDois = prompt("Em qual turno você estuda? Digite: M para Matutino, V para Vespertino ou N para Noturno")
+
+// switch (turnoDoAlunoDois){
+//     case "M" :
+//         console.log("Bom dia")
+//         break;
+//         case "V" :
+//             console.log("Boa tarde")
+//             break;
+//             case "N" :
+//                 console.log("Boa noite")
+//                 break;
+// }
+
+// 4.
+
+// let generoDoFilme = prompt("Qual genêro do filme?")
+// let preçoDoFilme = Number (prompt("Qual o preço do filme?"))
+
+// if (generoDoFilme === "fantasia" && preçoDoFilme < 15){
+//     console.log("Bom filme!")
+// }else{
+//     console.log("Escolha outro filme :(")
+// }
+
+// Desafios
+
+// 1.
+
+// let generoDoFilme = prompt("Qual genêro do filme?")
+// let preçoDoFilme = Number (prompt("Qual o preço do filme?"))
+// let lanchinhoUsuario = prompt("Qual snack você quer comprar?")
+
+// if (generoDoFilme === "fantasia" && preçoDoFilme < 15){
+//     console.log("Bom filme!")
+//     console.log(`Aproveite o seu ${lanchinhoUsuario}`)
+// }else{
+//     console.log("Escolha outro filme :(")
+// }
