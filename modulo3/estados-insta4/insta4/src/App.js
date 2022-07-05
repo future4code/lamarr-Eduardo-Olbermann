4b3b89b06b700f2550f9dd33d325a45e19d01e9f
EduardoFlossOlbermann <eduardoolbermann@hotmail.com>
2022-07-05 20:41:22 -0300

    exercicio

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,22 +1,74 @@
 import React from 'react';
+import Lista from './components/ListaPost';
 import Post from './components/Post/Post';
 import './style.css'
 
 
 function App() {
-return(
+
+  const Lista = () => {
+
+    const listaInformacoes = [
+        objetoPost,
+        objetoPostDois,
+        objetoPostTres,
+    ]
+
+    let listaDeInformacoesConvertida = listaInformacoes.map((item) => {
+        return <li>{item}</li>
+    })
+
+
+    return (
+
+        <div>
+            <ul>{listaDeInformacoesConvertida}</ul>
+        </div>
+    )
+
+
+}
+
+
+const objetoPost = {
+    nomeUsuario: 'paulinha',
+    fotoUsuario: 'https://picsum.photos/50/50',
+    fotoPost: 'https://picsum.photos/200/150'
+}
+const objetoPostDois = {
+    nomeUsuario2: 'eduardo',
+    fotoUsuario2: 'https://picsum.photos/50/50',
+    fotoPost2: 'https://picsum.photos/200/150'
+}
+
+const objetoPostTres = {
+    nomeUsuario3: 'pedrinho',
+    fotoUsuario3: 'https://picsum.photos/50/50',
+    fotoPost3: 'https://picsum.photos/200/150'
+}
+
+return (
   <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-            nomeUsuario2={'eduardo'}
-            fotoUsuario2={'https://picsum.photos/50/50'}
-            fotoPost2={'https://picsum.photos/200/150'}
-            nomeUsuario3={'pedrinho'}
-            fotoUsuario3={'https://picsum.photos/50/50'}
-            fotoPost3={'https://picsum.photos/200/150'}
-          />
+
+    <Lista/>
+
+    {/* <Post
+    
+
+    nomeUsuario={'paulinha'}
+    fotoUsuario={'https://picsum.photos/50/50'}
+    fotoPost={'https://picsum.photos/200/150'}
+
+
+    nomeUsuario2={'eduardo'}
+    fotoUsuario2={'https://picsum.photos/50/50'}
+    fotoPost2={'https://picsum.photos/200/150'}
+
+
+    nomeUsuario3={'pedrinho'}
+    fotoUsuario3={'https://picsum.photos/50/50'}
+    fotoPost3={'https://picsum.photos/200/150'}
+    /> */}
   </div>
 )
 
---
+++ modulo3/estados-insta4/insta4/src/components/ListaPost.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+// const Lista = () => {
+
+//     const listaInformacoes = [
+//         objetoPost,
+//         objetoPostDois,
+//         objetoPostTres,
+//     ]
+
+//     let listaDeInformacoesConvertida = listaInformacoes.map((item) => {
+//         return <li>{item}</li>
+//     })
+
+
+//     return (
+
+//         <div>
+//             <ul>{listaDeInformacoesConvertida}</ul>
+//         </div>
+//     )
+
+
+// }
+
+
+// const objetoPost = {
+//     nomeUsuario: 'paulinha',
+//     fotoUsuario: 'https://picsum.photos/50/50',
+//     fotoPost: 'https://picsum.photos/200/150'
+// }
+// const objetoPostDois = {
+//     nomeUsuario2: 'eduardo',
+//     fotoUsuario2: 'https://picsum.photos/50/50',
+//     fotoPost2: 'https://picsum.photos/200/150'
+// }
+
+// const objetoPostTres = {
+//     nomeUsuario3: 'pedrinho',
+//     fotoUsuario3: 'https://picsum.photos/50/50',
+//     fotoPost3: 'https://picsum.photos/200/150'
+// }
+
+export default Lista;
